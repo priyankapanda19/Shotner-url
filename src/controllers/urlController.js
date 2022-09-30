26f1f986114167b2b3df3f9fdbd82c0a5ff59ee6
Nareshn99 <nareshngohil99@gmail.com>
2022-09-30 12:05:03 +0530

    phase-1 complited

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,67 +1,74 @@
 const urlModel = require("../models/urlModel");
 const shortid = require("shortid");
 const valid = require("../validation/validators");
+const validUrl = require('valid-url')
+
 
 const createUrl = async (req, res) => {
   try {
     const requestBody = req.body;
-    const longUrl = requestBody.longUrl;
+    const { longUrl } = requestBody;
+
+    //validation for emptyBody
     if (Object.keys(requestBody).length == 0) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Please enter some input" });
-    }
-    if (!valid.isEmpty(longUrl)) {
-      return res
-        .status(400)
-        .send({ status: false, message: "longUrl is mandatory" });
-    }
-    if (!valid.isValidLink(longUrl)) {
-      return res
-        .status(400)
-        .send({ status: false, message: "longUrl is not in valid format" });
+      return res.status(400).send({ status: false, message: "Please enter some input" });
     }
 
+    //validation for longUrl
+    if (!valid.isEmpty(longUrl)) {
+      return res.status(400).send({ status: false, message: "longUrl is mandatory" });
+    }
+    if (!validUrl.isUri(longUrl)) {
+      return res.status(400).send({ status: false, message: 'Invalid base URL' })
+    }
     let uniqueUrl = await urlModel.findOne({ longUrl });
     if (uniqueUrl) {
-      return res
-        .status(400)
-        .send({ status: false, message: "LongUrl already exist" });
+      return res.status(409).send({ status: false, message: "LongUrl already shorted", shortUrl: uniqueUrl.shortUrl });
     }
+
+    //generate shortId and add in requestBody
     let sort = shortid.generate();
+    requestBody.urlCode = sort;
+
+    //validate uniqueness of urlCode
     let uniqueUrlCode = await urlModel.findOne({ urlCode: sort });
     if (uniqueUrlCode) {
-      return res
-        .status(400)
-        .send({ status: false, message: "urlCode already exist" });
+      return res.status(400).send({ status: false, message: "urlCode already exist" });
     }
-    requestBody.urlCode = sort;
-    let sortu = `http://localhost:3000/${sort}`;
-    requestBody.shortUrl = sortu;
+
+    //create shortUrl and add in requestBody
+    let shortUrl = `http://localhost:3000/${sort}`;
+    requestBody.shortUrl = shortUrl;
+
+    //create document amd send response
     await urlModel.create(requestBody);
-    const createUrl = await urlModel
-      .findOne({ shortUrl: sortu })
-      .select({ __v: 0 });
-    return res.status(201).send({ status: true, data: createUrl });
-  } catch (err) {
+    const createUrl = await urlModel.findOne({ shortUrl }).select({ _id: 0, __v: 0 });
+    return res.status(201).send({ status: true, date: createUrl });
+
+  }
+  catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
+
 };
+
+
+
 
 const findUrl = async (req, res) => {
   try {
-    const requestBody = req.params.urlCode;
-    // if (!valid.isValidObjectId(requestBody)) {
-    //   return res
-    //     .status(400)
-    //     .send({ status: false, message: "Not valid objectId" });
-    // }
-    const findUrl = await urlModel.findOne({urlCode:requestBody});
+    const urlCode = req.params.urlCode;
+
+    const findUrl = await urlModel.findOne({ urlCode });
     if (!findUrl) {
-      return res.status(404).send({ status: false, message: "URL not found" });
+      return res.status(404).send({ status: false, message: "shortUrl not found" });
     }
+
+    //redirect to longUrl
     return res.status(302).redirect(findUrl.longUrl);
-  } catch (err) {
+
+  }
+  catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
--- src/validation/validators.js
+++ src/validation/validators.js
@@ -1,24 +1,18 @@
-const mongoose=require ('mongoose')
+const mongoose = require('mongoose')
 
-const isEmpty = function (value) {
-    if (typeof value === "undefined" || value === null) return false;
-    if (typeof value === "string" && value.trim().length === 0) return false;
-    return true;
-  };
+const isEmpty = (value) => {
+  if (typeof value === "undefined" || value === null) return false;
+  if (typeof value === "string" && value.trim().length === 0) return false;
+  return true;
+};
 
-  const isValidLink=function(link){
-    return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(link)
-}
 
-const isValidObjectId = function (objectId) {
-  return mongoose.Types.ObjectId.isValid(objectId)
+const isValidObjectId = (data) => {
+  return mongoose.Types.ObjectId.isValid(data)
 }
 
 
 
-
-
-module.exports={
-    isEmpty,isValidLink,isValidObjectId
+module.exports = {
+  isEmpty, isValidObjectId
 }
-  
