56493d44f793c889160ecde199ca719112f500ec
priyanka <julypriyanka59@gmail.com>
2022-12-26 23:23:54 +0530

    bug free

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -97,7 +97,7 @@ const createUrl = async (req, res) => {
     //create document amd send response
     let data = await urlModel.create(requestBody);
     const createUrl = ({ urlCode: data.urlCode, longUrl: data.longUrl, shortUrl: data.shortUrl })
-    await SET_ASYNC(`${longUrl}`, 3600, JSON.stringify(createUrl))
+    await SET_ASYNC(`${longUrl}`, 60, JSON.stringify(createUrl))
     return res.status(201).send({ status: true, date: createUrl });
 
   }
